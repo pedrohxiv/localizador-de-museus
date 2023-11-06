4260d80fad2a4b00356d9533808ce8dbbeabea5f
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-11-06 01:57:41 -0300

    Requisito 06

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,15 +2,18 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -35,5 +38,18 @@ public class MuseumController {
     Museum museumCreated = this.service.createMuseum(museumModel);
     MuseumDto museumDto = ModelDtoConverter.modelToDto(museumCreated);
     return ResponseEntity.status(HttpStatus.CREATED).body(museumDto);
+  }
+
+  /**
+   * Get closest Museum GET method.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getClosestMuseum(
+      @RequestParam double lat,
+      @RequestParam double lng,
+      @RequestParam(name = "max_dist_km") double maxDistanceKm) {
+    Museum museum = this.service.getClosestMuseum(new Coordinate(lat, lng), maxDistanceKm);
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(museum);
+    return ResponseEntity.status(HttpStatus.OK).body(museumDto);
   }
 }
